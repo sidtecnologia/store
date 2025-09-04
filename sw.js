92d9c3d9047e398cbdf3c653323f58b89537d3e5
sitecnologia <sidsoporte@proton.me>
2025-09-03 20:57:25 -0500

    updt store imgs

--- sw.js
+++ sw.js
@@ -6,23 +6,6 @@ const urlsToCache = [
     '/js/app.js',
     '/js/data.js',
     '/favicon.png',
-    '/img/bolso-piel.jpg',
-    '/img/camisa-lino.jpg',
-    '/img/chaqueta-cuero.jpg',
-    '/img/gorra-clasica.jpg',
-    '/img/jeans.jpg',
-    '/img/pants-deportivos.jpg',
-    '/img/sudadera.jpg',
-    '/img/tenis-urbanos.jpg',
-    '/img/vestido-floral.jpg',
-    '/img/zapatos-tacon.jpg',
-    '/img/vestido-floral-1.jpg',
-    '/img/vestido-floral-2.jpg',
-    '/img/vestido-floral-3.jpg',
-    '/img/chaqueta-cuero-1.jpg',
-    '/img/chaqueta-cuero-2.jpg',
-    '/img/pants-deportivos-1.jpg',
-    '/img/bolso-piel-1.jpg'
 ];
 
 self.addEventListener('install', event => {
